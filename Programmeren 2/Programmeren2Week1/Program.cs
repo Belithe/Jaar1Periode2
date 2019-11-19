c98a0ef170c13f90092db99d782ad9f8ac5d7212
Gengar2 <Junkmailbelith@gmail.com>
2019-11-19 20:45:48 +0100

    Catchup temp Progremmeren map

---
+++ Programmeren 2/Programmeren2Week1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Opdracht0
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            int getal = LeesInt("tik een getal: ");
+            Console.WriteLine("Je hebt {0} ingetikt.", getal);
+
+            int leeftijd = LeesInt("hoe oud ben je? ", 0, 120);
+            Console.WriteLine("Je bent {0} jaar oud.", leeftijd);
+
+            string naam = LeesString("Hoe heet je? ");
+            Console.WriteLine("Aangenaam kennis met je te maken, {0}.", naam);
+            Console.ReadKey();
+        }
+
+        int LeesInt(string vraag)
+        {
+            Console.Write(vraag);
+            int getalInput = int.Parse(Console.ReadLine());
+            return getalInput;
+        }
+
+        int LeesInt(string vraag, int min, int max)
+        {
+            Console.Write(vraag);
+            int leeftijd = int.Parse(Console.ReadLine());
+            while(leeftijd <= min || leeftijd > max) {
+                Console.Write(vraag);
+                leeftijd = int.Parse(Console.ReadLine());
+            }
+
+            return leeftijd;
+        }
+
+        string LeesString(string vraag)
+        {
+            Console.Write(vraag);
+            string naam = Console.ReadLine();
+            return naam;
+        }
+
+    }
+}
