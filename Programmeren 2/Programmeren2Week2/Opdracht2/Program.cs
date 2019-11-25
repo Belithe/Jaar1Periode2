edfe38387f16738dbaf27b3e4822b25fc412a9b7
Unknown <JunkmailBelith@gmail.com>
2019-11-26 00:08:34 +0100

    Programmeren2Week2 Opdr 1 + 2 klaar

---
+++ Programmeren 2/Programmeren2Week2/Opdracht2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        void Start()
+        {
+            int[,] matrix = new int[15, 10];
+            int numberToSearch;
+            Positie pos;
+
+            InitMatrixRandom(matrix, 1, 100);
+            PrintMatrix(matrix);
+
+            Console.Write("\nGeef een int waarde: ");
+            numberToSearch = int.Parse(Console.ReadLine());
+
+            pos = ZoekGetal(matrix, numberToSearch);
+
+            if (pos.found)
+                Console.Write(String.Format("\nForwards: {0,2},{1}", pos.rij + 1, pos.kolom + 1));
+            else
+                Console.Write("\nGetal niet gevonden!");
+
+            pos = ZoekGetalAchterwaards(matrix, numberToSearch);
+
+            if (pos.found)
+                Console.Write("\nBackwards: {0,2},{1}", pos.rij + 1, pos.kolom + 1);
+            else
+                Console.Write("\nGetal niet gevonden!");
+        }
+
+        void InitMatrixRandom(int[,] matrix, int min, int max)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+        void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(String.Format("{0,2} ", matrix[i, j].ToString()));
+                }
+
+                Console.WriteLine();
+            }
+        }
+        Positie ZoekGetal(int[,] matrix, int zoekGetal)
+        {
+            Positie search = new Positie();
+
+            for (int i = 0; i < matrix.GetLength(0) && !search.found; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) && !search.found; j++)
+                    if (matrix[i, j] == zoekGetal)
+                    {
+                        search.found = true;
+                        search.rij = j;
+                        search.kolom = i;
+                    }
+            }
+            return search;
+        }
+
+        Positie ZoekGetalAchterwaards(int[,] matrix, int zoekGetal)
+        {
+            Positie search = new Positie();
+
+            for (int i = matrix.GetLength(0) - 1; i >= 0 && !search.found; i--)
+                for (int j = matrix.GetLength(1) - 1; j >= 0 && !search.found; j--)
+                    if (matrix[i, j] == zoekGetal)
+                    {
+                        search.found = true;
+                        search.rij = i;
+                        search.kolom = j;
+                    }
+
+            return search;
+        }
+
+    }
+}
--- Programmeren 2/Programmeren2Week2/Programmeren2Week2/Programmeren2Week2/Program.cs
+++ Programmeren 2/Programmeren2Week2/Programmeren2Week2/Programmeren2Week2/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Programmeren2Week2
+namespace Opdracht1
 {
     class Program
     {
@@ -12,9 +12,9 @@ namespace Programmeren2Week2
 
         void Start()
         {
-            int[,] matrix = new int[12, 10];
-            InitMatrix2D(matrix);
-            PrintMatrix(matrix);
+            int[,] matrix = new int[10, 10];
+            InitMatrixLineair(matrix);
+            PrintMatrixWithCross(matrix);
         }
 
         void PrintMatrix(int[,] matrix)
@@ -23,7 +23,7 @@ namespace Programmeren2Week2
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    Console.Write(String.Format("{0, 4} ", matrix[i, j].ToString()));
+                    Console.Write(String.Format("{0, 3} ", matrix[i, j].ToString()));
                 }
 
                 Console.WriteLine();
@@ -42,5 +42,47 @@ namespace Programmeren2Week2
                 }
             }
         }
+
+        void InitMatrixLineair(int[,] matrix)
+        {
+            int row = 0;
+            int column = 0;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[row,column] = i + 1;
+                column++;
+                if (column == matrix.GetLength(1))
+                {
+                    column = 0;
+                    row++;
+                }
+            }
+        }
+        void PrintMatrixWithCross(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.ResetColor();
+
+                    if (i == j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    if(Math.Abs(j - matrix.GetLength(1)+1) == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    }
+
+                    Console.Write("{0, 3}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+
+
     }
 }
